f066fcb9a3d067a74634cf8c97bdfe5ed131b6e7
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-08 18:38:45 -0700

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 import ContributorList from './Components/ContributorList';
+
+function simpleForm(){
+
+  return (
+    <div className="input-field">
+        <form onSubmit={this.handleSubmit}>
+          <input placeholder="Top 5 Individuals and Business" id="amount" type="number" className="validate" value={this.state.value} onChange={this.handleChange} />
+          <label for="amount">How many would you like to see?</label>
+          <input type="submit" value="Submit" />
+        </form>
+     </div>)
+}
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {number:20}
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+  //state is changing or set with every key stroke entry into the input field
+  //you can have a conditional statement to prevent it from setting state until certain conditions are met.
+    handleChange(event) {
+      this.setState({number: event.target.value});
+    }
+  //alert the user on what the new state is set to
+    handleSubmit(event) {
+      console.log("submitted");
+      console.log(this.state.number)
+
+      event.preventDefault();
+    }
+
   render() {
     return (
       <div>
-        <h1>Top Contributors to Political Campaigns in Oregon</h1>
-        <ContributorList/>
+        <div className="headline">
+          <h1>Top Contributors to Political Campaigns in Oregon</h1>
+
+        </div>
+        <div className="row">
+          <div className="col m6">
+            <h2>Individuals</h2>
+            <ContributorList type="individual" number={this.state.number}/>
+          </div>
+          <div className="col m6">
+            <h2>Businesses</h2>
+
+            <ContributorList type="business" number={this.state.number}/>
+          </div>
+        </div>
+        <div className="headline">
+          <h1> </h1>
+        </div>
       </div>
     );
   }
--- src/Components/ContributorList.js
+++ src/Components/ContributorList.js
@@ -1,25 +1,42 @@
 import React from 'react';
-
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 class ContributorList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      type: this.props.type,
+      number: this.props.number,
       contributors:[]
     };
   }
-  componentWillMount(){
-    fetch('http://54.213.83.132/hackoregon/http/oregon_individual_contributors/5/')
+  componentWillMount(type){
+    fetch('http://54.213.83.132/hackoregon/http/oregon_'+this.state.type+'_contributors/'+this.state.number+'/')
       .then((response)=> response.json())
       .then((data)=> this.setState({contributors: data}));
   }
 
   renderContributors(){
-    return this.state.contributors.map(contributor => <li key={contributor.sum}>{contributor.contributor_payee}: ${contributor.sum}</li>)
+    return this.state.contributors.map(contributor =>
+      <li className={this.props.type} key={contributor.sum}>
+        <div className="card  blue darken-4">
+            <div className="card-content white-text">
+              <span className="card-title"><h4>{contributor.contributor_payee}</h4></span>
+              <p>In the previous election {contributor.contributor_payee} contributed</p>
+              <h5>
+                $ {numberWithCommas(parseInt(contributor.sum))}.00
+              </h5>
+            </div>
+
+          </div>
+      </li>)
   }
 
   render(){
+    const {type} = this.props;
     return(
-      <div>
+      <div className="{this.props.type}Container">
       <ol>
       {this.renderContributors()}
       </ol>
