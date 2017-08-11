04129c626a955657a50f76586ffd313e25865b0a
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-10 20:25:13 -0700

    refactored to return list based on user input

--- src/App.js
+++ src/App.js
@@ -1,54 +1,72 @@
 import React, { Component } from 'react';
 import ContributorList from './Components/ContributorList';
 
-function simpleForm(){
 
-  return (
-    <div className="input-field">
-        <form onSubmit={this.handleSubmit}>
-          <input placeholder="Top 5 Individuals and Business" id="amount" type="number" className="validate" value={this.state.value} onChange={this.handleChange} />
-          <label for="amount">How many would you like to see?</label>
-          <input type="submit" value="Submit" />
-        </form>
-     </div>)
-}
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {number:20}
+    this.state = {
+      number:5,
+      business: {},
+      Individual: {},
+      value: 6
+    }
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.hitServer = this.hitServer.bind(this);
+    }
+
+    hitServer(){
+      fetch('http://54.213.83.132/hackoregon/http/oregon_business_contributors/'+this.state.number+'/')
+        .then((response)=> response.json())
+        .then((data)=> this.setState({business: data}));
+      fetch('http://54.213.83.132/hackoregon/http/oregon_individual_contributors/'+this.state.number+'/')
+        .then((response)=> response.json())
+        .then((data)=> this.setState({individual: data}));
+    }
+
+    componentWillMount(){
+      console.log("will Mount");
+      this.hitServer();
     }
 
   //state is changing or set with every key stroke entry into the input field
   //you can have a conditional statement to prevent it from setting state until certain conditions are met.
-    handleChange(event) {
-      this.setState({number: event.target.value});
+    onEnter(e){
+      e.preventDefault();
     }
-  //alert the user on what the new state is set to
-    handleSubmit(event) {
-      console.log("submitted");
-      console.log(this.state.number)
 
-      event.preventDefault();
+    handleChange(e) {
+      console.log("changing");
+      if ((e.target.value !== this.state.value)&&(e.target.value !== "")) {
+        this.setState({ number: e.target.value });
+      }
     }
+    handleSubmit(event) {
+          event.preventDefault();
+          this.hitServer();
+        }
 
   render() {
     return (
       <div>
         <div className="headline">
           <h1>Top Contributors to Political Campaigns in Oregon</h1>
-
+          <div className="input-field">
+              <form onSubmit={this.handleSubmit}>
+                <input placeholder="Top 5 Individuals and Business" id="amount" type="number" className="validate" defaultValue={this.state.number} onChange={this.handleChange} />
+                <label for="amount">How many would you like to see?</label>
+                <a className="blue darken-4 btn" onClick={this.handleSubmit}>Submit</a>
+              </form>
+           </div>
         </div>
         <div className="row">
           <div className="col m6">
-            <h2>Individuals</h2>
-            <ContributorList type="individual" number={this.state.number}/>
+            <ContributorList type="Individual" list={this.state.individual}/>
           </div>
           <div className="col m6">
-            <h2>Businesses</h2>
-
-            <ContributorList type="business" number={this.state.number}/>
+            <ContributorList type="Business" list={this.state.business}/>
           </div>
         </div>
         <div className="headline">
--- src/Components/ContributorList.js
+++ src/Components/ContributorList.js
@@ -2,44 +2,45 @@ import React from 'react';
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function renderContributors(list, type){
+  try{
+  return list.map(contributor =>
+    <li className={type.toLowerCase()} key={contributor.contributor_payee}>
+      <div className="card  blue darken-4">
+          <div className="card-content white-text">
+            <span className="card-title"><h4>{contributor.contributor_payee}</h4></span>
+            <p>In the previous election {contributor.contributor_payee} contributed</p>
+            <h5>
+              $ {numberWithCommas(parseInt(contributor.sum))}.00
+            </h5>
+        </div>
+
+        </div>
+    </li>)
+  } catch(e){
+    console.log(e);
+  }
+}
 class ContributorList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      type: this.props.type,
-      number: this.props.number,
       contributors:[]
     };
-  }
-  componentWillMount(type){
-    fetch('http://54.213.83.132/hackoregon/http/oregon_'+this.state.type+'_contributors/'+this.state.number+'/')
-      .then((response)=> response.json())
-      .then((data)=> this.setState({contributors: data}));
-  }
-
-  renderContributors(){
-    return this.state.contributors.map(contributor =>
-      <li className={this.props.type} key={contributor.sum}>
-        <div className="card  blue darken-4">
-            <div className="card-content white-text">
-              <span className="card-title"><h4>{contributor.contributor_payee}</h4></span>
-              <p>In the previous election {contributor.contributor_payee} contributed</p>
-              <h5>
-                $ {numberWithCommas(parseInt(contributor.sum))}.00
-              </h5>
-            </div>
-
-          </div>
-      </li>)
-  }
+ }
+ componentWillReceiveProps(nextProps) {
+   this.setState({ contributors: nextProps.list });
+ }
 
   render(){
     const {type} = this.props;
     return(
       <div className="{this.props.type}Container">
-      <ol>
-      {this.renderContributors()}
-      </ol>
+        <h2>{type}</h2>
+        <ol>
+          {renderContributors(this.props.list, this.props.type)}
+        </ol>
       </div>
     )
   }
